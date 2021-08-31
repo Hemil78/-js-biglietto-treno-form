de9998fe1e7eef3457810b432e22861efc021303
Hemil78 <zanattahemil@gmail.com>
2021-08-31 10:53:07 +0200

    correzioni

--- js/script.js
+++ js/script.js
@@ -8,39 +8,48 @@ btnGenera.addEventListener ( "click",
 
         //prendo i valori dei campi input e select
         var name = document.getElementById("name").value;
-        var km = document.getElementById("km").value;
+        var km = parseInt(document.getElementById("km").value);
         var fascia = document.getElementById("fascia").value;
 
-        //calcolo il prezzo in base all'età
-        var prezzoKm = km * 0.21;
+        if(name != "" && !isNaN(km) && km > 0 && fascia != "") {
 
-        if (fascia == "minorenne") {
-            //sconto 20%
-            prezzoKm = (prezzoKm - ((prezzoKm / 100) * 20));
-        } else if (fascia == "over"){
-            //sconto 40%
-            prezzoKm = (prezzoKm - ((prezzoKm / 100) * 40));
+            
+            //calcolo il prezzo in base all'età
+            var prezzoKm = km * 0.21;
+            var tipoOfferta = "Biglietto Standard";
+            
+            if (fascia == "minorenne") {
+                //sconto 20%
+                prezzoKm = (prezzoKm - ((prezzoKm / 100) * 20));
+                tipoOfferta = "Sconto Minorenne";
+            } else if (fascia == "over"){
+                //sconto 40%
+                prezzoKm = (prezzoKm - ((prezzoKm / 100) * 40));
+                tipoOfferta = "Sconto Silver";
+            } else {
+                //prezzo fisso
+                prezzoKm = km * 0.21;
+            }
+            //stampo a video tutti i dati nel div del biglietto
+            // nome del passeggero
+            document.getElementById("nome-passeggero").innerHTML = name;
+            //tipo di offerta
+            document.getElementById("tipo-offerta").innerHTML = tipoOfferta;
+            //prezzo
+            document.getElementById('totale').innerHTML = prezzoKm.toFixed(2) + " €";
+            //creare e stampo a video  numero carrozza e codice cliente
+            var numCarrozza = Math.floor(Math.random() * 10) + 1;
+            document.getElementById("carrozza").innerHTML = numCarrozza;
+            
+            var numCodice = Math.floor(Math.random() * (99999 - 90000)) + 90000;
+            document.getElementById("codice").innerHTML = numCodice;
+            //mostro il biglietto
+            document.getElementById("biglietto").classList.add("open");
         } else {
-            //prezzo fisso
-            prezzoKm = km * 0.21;
+            alert ("errore!");
         }
-        //stampo a video tutti i dati nel div del biglietto
-        // nome del passeggero
-        document.getElementById("nome-passeggero").innerHTML = name;
-        //tipo di offerta
-        document.getElementById("tipo-offerta").innerHTML = fascia;
-        //prezzo
-        document.getElementById('totale').innerHTML = prezzoKm.toFixed(2);
-        //creare e stampo a video  numero carrozza e codice cliente
-        var numCarrozza = Math.floor(Math.random() * 10) + 1;
-        document.getElementById("carrozza").innerHTML = numCarrozza;
-
-        var numCodice = Math.floor(Math.random() * 100000) + 1;
-        document.getElementById("codice").innerHTML = numCodice;
-        //mostro il biglietto
-        document.getElementById("biglietto").classList.add("open");
-        //tolgo i puntini
-        document.getElementById("points").innerHTML = "";
+            
+            
     }
 );
 
@@ -53,7 +62,7 @@ btnAnnulla.addEventListener("click",
         //reset di tutti i campi
         document.getElementById("name").value = "";
         document.getElementById("km").value = "";
-        document.getElementById("fascia").value = "";
+        document.getElementById("seleziona").selectedIndex = "0";
         
     }
 );
